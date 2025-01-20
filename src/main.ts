eae44153a8fe839166307ed5162149dc33ed8fde
amanullah7649 <aullah060@gmail.com>
2025-01-21 00:33:57 +0600

    Add configuration support with dotenv and update app module for global config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ConfigService } from '@nestjs/config'; // Import ConfigService from NestJS
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const appConfig = app.get(ConfigService);  // Use ConfigService properly
+  const appName = appConfig.get<string>('APP_NAME');
+  const appPort = appConfig.get<number>('APP_PORT') || 3001;
+
+  console.log(`Application Name: ${appName}`);  // Output the app name
+  console.log(`Application is running on port: ${appPort}`);
+
+  await app.listen(appPort);  // Listen on the specified port
+  console.log(`Application is running on: ${await app.getUrl()}`);  // Log the app URL
 }
+
 bootstrap();
